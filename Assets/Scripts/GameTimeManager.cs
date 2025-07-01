5c698f7b28584f7266c8a83b5c79350e512f2cf7
hanzc <hanerlv@gmail.com>
2025-07-01 13:20:09 +0800

    完成打招呼

--- Assets/Scripts/GameTimeManager.cs
+++ Assets/Scripts/GameTimeManager.cs
@@ -2,17 +2,25 @@ using UnityEngine;
 
 public class GameTimeManager : MonoBehaviour
 {
-    public static GameTimeManager Instance;
+    public static GameTimeManager Instance { get; private set; }
     public int Hour { get; private set; }
     public int Minute { get; private set; }
 
     float timeCounter = 0f;
     public float minuteLength = 1f; // 1秒=1游戏分钟
 
-    void Awake()
+    private void Awake()
     {
+        // 保证只有一个实例存在
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject); // 场景里重复就销毁多余的
+            return;
+        }
         Instance = this;
-        Hour = 6; Minute = 0;
+        Hour = 9; Minute = 0;
+        // DontDestroyOnLoad(gameObject); // 如果想切场景不丢失，就加上
+        DontDestroyOnLoad(gameObject);
     }
 
     void Update()
